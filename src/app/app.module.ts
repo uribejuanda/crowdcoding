0fd5bf8bc0ff0428cb13d8da04695c51093007e5
Juanda Uribe <j.uribe@globant.com>
2017-11-16 20:35:39 -0500

    dark theme approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
@@ -8,14 +9,17 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
